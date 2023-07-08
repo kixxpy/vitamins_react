8a4d8243d7694d60e53e9413dc5c9a97d0bd9da2
kirillEN <81442482+kixxpy@users.noreply.github.com>
2023-07-09 01:48:25 +0300

    card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import { Card } from './components/Card';
 import { Header } from './components/Header';
 import { Drawer } from './components/Drawer';
+const arr = [
+	{ name: 'Витамины для взрослых и детей, Омега-3', imgUrl: './img/vitamins/1.jpg', price: 413 },
+	{
+		name: 'Витамины для энергии, красоты и иммунитета, Омега 3-6-9',
+		imgUrl: './img/vitamins/2.jpg',
+		price: 412,
+	},
+	{
+		name: 'Комплекс витаминов группы Б, от Б1 до Б12',
+		imgUrl: './img/vitamins/3.jpg',
+		price: 373,
+	},
+	{
+		name: 'Кальций Д3, витамины для укрепления костей, волос, ногтей',
+		imgUrl: './img/vitamins/4.jpg',
+		price: 287,
+	},
+	{
+		name: 'Йод органический 1000 мкг',
+		imgUrl: './img/vitamins/5.jpg',
+		price: 271,
+	},
+	{
+		name: 'Хром Пиколинат 250 мкг, витамины для женщин',
+		imgUrl: './img/vitamins/6.jpg',
+		price: 317,
+	},
+];
 function App() {
 	return (
 		<div className="wrapper clear">
@@ -17,12 +45,9 @@ function App() {
 					</div>
 				</div>
 				<div className="vitamins">
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
+					{arr.map((obj) => (
+						<Card name={obj.name} imgUrl={obj.imgUrl} price={obj.price} />
+					))}
 				</div>
 			</div>
 		</div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
-export const Card = () => {
+export const Card = (props) => {
 	return (
 		<div className="card">
-		<img className="favorit" src="./img/heard-unliked.svg" alt="heard" />
-		<img width={134} height={143} src="./img/vitamins/1.jpg" alt="vitamins" />
-		<h5>Витамины для взрослых и детей Омега-3</h5>
+		<img className="favorit" src={"./img/heard-unliked.svg"} alt="heard" />
+		<img width={134} height={143} src={props.imgUrl} alt="vitamins" />
+		<h5>{props.name}</h5>
 		<div className="cardPrice">
 			<div className="price">
 				<span>Цена:</span>
-				<b>413 руб.</b>
+				<b>{props.price} руб.</b>
 			</div>
 			<img className="button" src="./img/plus.svg" alt="plus" />
 		</div>
